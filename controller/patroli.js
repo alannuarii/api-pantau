ff88db4e96fee89930208c6114432394d786305d
sierrawebid <sierrawebid@gmail.com>
2023-08-30 07:20:23 +0800

    Update

--- controller/patroli.js
+++ controller/patroli.js
@@ -5,8 +5,11 @@ const dbQuery = promisify(db.query).bind(db);
 
 const insertPatroli = async (req, res) => {
   try {
-    const { foto, lokasi, catatan } = req.body;
+    const { foto, nama, shift, waktu, lokasi, catatan } = req.body;
     console.log(foto);
+    console.log(nama);
+    console.log(shift);
+    console.log(waktu);
     console.log(lokasi);
     console.log(catatan);
     res.status(200).json({ message: "Berhasil mengambil data", data: [] });
