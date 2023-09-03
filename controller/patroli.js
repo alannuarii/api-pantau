0fdccc397b3eadebf67836246ae5ac675c996e47
sierrawebid <sierrawebid@gmail.com>
2023-09-03 13:14:27 +0800

    Update

--- controller/patroli.js
+++ controller/patroli.js
@@ -8,13 +8,33 @@ const dbQuery = promisify(db.query).bind(db);
 const insertPatroli = async (req, res) => {
   try {
     const { foto, nama, shift, waktu, lokasi, catatan } = req.body;
-    const targetDir = path.join(__dirname, "..", "static", "img", "patroli");
-    const fileName = `${new Date(waktu).getTime()}.jpeg`;
-    convertBase64ToFile(foto, targetDir, fileName)
-    res.status(200).json({ message: "Berhasil mengambil data", data: [] });
+    // const targetDir = path.join(__dirname, "..", "static", "img", "patroli");
+    // const fileName = `${new Date(waktu).getTime()}.jpeg`;
+    // const sql = "INSERT INTO patroli (nama, waktu, shift, lokasi, foto, catatan) VALUES (?, ?, ?, ?, ?, ?)";
+    // const values = [nama, waktu, shift, lokasi, fileName, catatan];
+    // await dbQuery(sql, values);
+    // convertBase64ToFile(foto, targetDir, fileName);
+    res.status(200).json({ message: "Berhasil", data: { nama, shift, waktu, lokasi } });
   } catch (error) {
-    console.log(error);
+    console.error("Kesalahan koneksi ke database:", error);
     res.status(500).json({ message: "Gagal input data", data: [] });
+  } finally {
+    if (db) {
+      db.end();
+    }
+  }
+};
+
+const getDataPatroli = async (req, res) => {
+  try {
+    const sql = "SELECT * FROM patroli WHERE ";
+  } catch (error) {
+    console.error("Kesalahan koneksi ke database:", error);
+    res.status(500).json({ message: "Gagal input data", data: [] });
+  } finally {
+    if (db) {
+      db.end();
+    }
   }
 };
 
