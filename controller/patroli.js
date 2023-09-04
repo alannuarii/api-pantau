895be289b160c428d046acd194e994b540a7824f
sierrawebid <sierrawebid@gmail.com>
2023-09-04 10:15:46 +0800

    Update

--- controller/patroli.js
+++ controller/patroli.js
@@ -7,14 +7,15 @@ const dbQuery = promisify(db.query).bind(db);
 
 const insertPatroli = async (req, res) => {
   try {
-    const { foto, nama, shift, waktu, lokasi, catatan } = req.body;
-    // const targetDir = path.join(__dirname, "..", "static", "img", "patroli");
-    // const fileName = `${new Date(waktu).getTime()}.jpeg`;
-    // const sql = "INSERT INTO patroli (nama, waktu, shift, lokasi, foto, catatan) VALUES (?, ?, ?, ?, ?, ?)";
-    // const values = [nama, waktu, shift, lokasi, fileName, catatan];
-    // await dbQuery(sql, values);
-    // convertBase64ToFile(foto, targetDir, fileName);
-    res.status(200).json({ message: "Berhasil", data: { nama, shift, waktu, lokasi } });
+    const { kode, foto, nama, shift, waktu, lokasi, catatan } = req.body;
+    const targetDir = path.join(__dirname, "..", "static", "img", "patroli");
+    const fileName = `${new Date(waktu).getTime()}.jpeg`;
+    const sql = "INSERT INTO patroli (kode, nama, waktu, shift, lokasi, foto, catatan) VALUES (?, ?, ?, ?, ?, ?, ?)";
+    const values = [kode, nama, waktu, shift, lokasi, fileName, catatan];
+    await dbQuery(sql, values);
+    convertBase64ToFile(foto, targetDir, fileName);
+    const dataPatroli = await getDataPatroli(kode);
+    res.status(200).json({ message: "Berhasil", data: dataPatroli });
   } catch (error) {
     console.error("Kesalahan koneksi ke database:", error);
     res.status(500).json({ message: "Gagal input data", data: [] });
@@ -25,16 +26,14 @@ const insertPatroli = async (req, res) => {
   }
 };
 
-const getDataPatroli = async (req, res) => {
+const getDataPatroli = async (kode) => {
   try {
-    const sql = "SELECT * FROM patroli WHERE ";
+    const sql = "SELECT * FROM patroli WHERE kode = ?";
+    const values = [kode];
+    const result = await dbQuery(sql, values);
+    return result;
   } catch (error) {
     console.error("Kesalahan koneksi ke database:", error);
-    res.status(500).json({ message: "Gagal input data", data: [] });
-  } finally {
-    if (db) {
-      db.end();
-    }
   }
 };
 
