5a31f6b8caccb0b362bf34bd131f52b879c55f1f
sierrawebid <sierrawebid@gmail.com>
2023-10-10 06:48:47 +0800

    Update

--- db/connection.js
+++ db/connection.js
@@ -1,35 +1,12 @@
 const mysql = require("mysql");
 require("dotenv").config();
 
-let db;
-
-function handleDisconnect() {
-  db = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    connectionLimit: 10,
-  });
-
-  db.connect((err) => {
-    if (err) {
-      console.error("Error connecting to MySQL:", err);
-      setTimeout(handleDisconnect, 2000); // Coba reconnect setelah 2 detik
-    }
-    console.log("Connected to MySQL");
-  });
-
-  db.on("error", (err) => {
-    console.error("MySQL error:", err);
-    if (err.code === "ECONNRESET") {
-      handleDisconnect();
-    } else {
-      throw err;
-    }
-  });
-}
-
-handleDisconnect();
+const db = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  connectionLimit: 10,
+});
 
 module.exports = db;
