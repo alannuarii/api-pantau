9029fd2fad7144445250900c1158b71b6baaa812
sierrawebid <sierrawebid@gmail.com>
2023-10-11 07:04:38 +0800

    Update

--- controller/tamu.js
+++ controller/tamu.js
@@ -6,12 +6,13 @@ const dbQuery = promisify(db.query).bind(db);
 const insertTamu = async (req, res) => {
   try {
     const { foto, nama, instansi, nohp, tujuan } = req.body;
-    console.log(foto);
-    console.log(nama);
-    console.log(instansi);
-    console.log(nohp);
-    console.log(tujuan);
-    res.status(200).json({ message: "Berhasil mengambil data", data: [] });
+    const targetDir = path.join(__dirname, "..", "static", "img", "tamu");
+    const fileName = `${nama.replace(/ /g, "")}-${new Date(waktu).getTime()}.jpeg`;
+    const sql = "INSERT INTO tamu (nama, instansi, nohp, tujuan, foto) VALUES (?, ?, ?, ?, ?)";
+    const values = [nama, instansi, nohp, tujuan, fileName];
+    await dbQuery(sql, values);
+    await convertBase64ToFile(foto, targetDir, fileName);
+    res.status(200).json({ message: "Berhasil", data: { nama } });
   } catch (error) {
     console.log(error);
     res.status(500).json({ message: "Gagal input data", data: [] });
